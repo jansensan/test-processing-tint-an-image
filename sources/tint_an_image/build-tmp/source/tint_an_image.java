959013379bb976eb9614edceab316f71da6de544
Mat Janson Blanchet <mat.jb@jansensan.net>
2013-07-28 13:18:04 -0400

    found the solution to tint a b&w image!

--- sources/tint_an_image/build-tmp/source/tint_an_image.java
+++ sources/tint_an_image/build-tmp/source/tint_an_image.java
@@ -14,29 +14,81 @@ import java.io.IOException;
 
 public class tint_an_image extends PApplet {
 
-int BG_COLOR = color(240, 240, 240);
-String IMG_URL = "images/paterson-drawing-320x320.png";
-String FLAT_COLOR_BLOCK_URL = "images/white-320x320.png";
+int BG_COLOR = color(160, 160, 160);
+int LINES_COLOR = color(80, 80, 80);
+int FILL_COLOR = color(224, 224, 224);
+String IMG_URL = "images/paterson-drawing.png";
+
+int centerX;
+int centerY;
+
+PImage source;
+
+int[] targetDimensions;
+PImage linesImage;
+PImage fillImage;
 
 
-PImage linesColor;
-PImage fillColor;
-
-PImage fillMask;
-
+//===========/----------------------------------------------
+//  [_PRO]  /  Processing
+//=========/------------------------------------------------
 
 public void setup()
 {
 	size(640, 640, P2D);
 	background(BG_COLOR);
 
-	// set lines
-	linesColor = loadImage(IMG_URL);
-	linesColor.filter(INVERT);
+	//
+	centerX = PApplet.parseInt(width * 0.5f);
+	centerY = PApplet.parseInt(height * 0.5f);
 
-	// set fill
-	fillColor = loadImage(FLAT_COLOR_BLOCK_URL);
-	fillMask = loadImage(IMG_URL);
+	// set source
+	source = loadImage(IMG_URL);
+	source.loadPixels();
+
+	// var relative to source
+	int numPixels = source.pixels.length;
+	setTargetDimensions(source.width, source.height);
+
+	// instantiate fill color
+	linesImage = createImage	(
+								source.width, 
+								source.height,
+								ARGB
+							);
+	linesImage.loadPixels();
+
+	// instantiate lines color
+	fillImage = createImage	(
+									source.width, 
+									source.height,
+									ARGB
+								);
+	fillImage.loadPixels();
+
+	// loop through source
+	int threshold = 128;
+	for(int i = 0; i < numPixels; i++)
+	{
+		// fill color
+		linesImage.pixels[i] = color	(
+											red(LINES_COLOR),
+											green(LINES_COLOR),
+											blue(LINES_COLOR),
+											alpha(source.pixels[i])
+										); 
+
+		// lines color
+		if	(
+				red(source.pixels[i]) > threshold 
+				&& green(source.pixels[i]) > threshold
+				&& blue(source.pixels[i]) > threshold
+			)
+		{
+			fillImage.pixels[i] = FILL_COLOR;
+		}
+	}
+	linesImage.updatePixels();
 }
 
 
@@ -44,16 +96,47 @@ public void draw()
 {
 	background(BG_COLOR);
 
-	// draw lines
-	// image(linesColor, width * 0.5, height * 0.5);
-	// tint(240, 0, 0);
-
-	// draw colors
-	image(fillColor, width * 0.5f, height * 0.5f);
-	fillColor.mask(fillMask);
-	tint(0, 0, 240);
+	image	(
+				linesImage, 
+				centerX, 
+				centerY,
+				targetDimensions[0],
+				targetDimensions[1]
+			);
+	image	(
+				fillImage, 
+				centerX, 
+				centerY,
+				targetDimensions[0],
+				targetDimensions[1]
+			);
 
 	imageMode(CENTER);
+}
+
+
+//===========/----------------------------------------------
+//  [_MTD]  /  Methods
+//=========/------------------------------------------------
+
+public void setTargetDimensions(int w, int h)
+{
+	targetDimensions = new int[2];
+
+	float MAX_SOURCE_SIZE = 1920;
+	float TARGET_SIZE = 480;
+	float RATIO = TARGET_SIZE / MAX_SOURCE_SIZE;
+
+	if(w > h)
+	{
+		targetDimensions[0] = PApplet.parseInt(TARGET_SIZE);
+		targetDimensions[1] = PApplet.parseInt(h * RATIO);
+	}
+	else
+	{
+		targetDimensions[0] = PApplet.parseInt(w * RATIO);
+		targetDimensions[1] = PApplet.parseInt(TARGET_SIZE);
+	}
 }
   static public void main(String[] passedArgs) {
     String[] appletArgs = new String[] { "tint_an_image" };
--- sources/tint_an_image/tint_an_image.pde
+++ sources/tint_an_image/tint_an_image.pde
@@ -1,26 +1,78 @@
-color BG_COLOR = color(240, 240, 240);
-String IMG_URL = "images/paterson-drawing-320x320.png";
-String FLAT_COLOR_BLOCK_URL = "images/white-320x320.png";
+color BG_COLOR = color(160, 160, 160);
+color LINES_COLOR = color(80, 80, 80);
+color FILL_COLOR = color(224, 224, 224);
+String IMG_URL = "images/paterson-drawing.png";
+
+int centerX;
+int centerY;
+
+PImage source;
+
+int[] targetDimensions;
+PImage linesImage;
+PImage fillImage;
 
 
-PImage linesColor;
-PImage fillColor;
-
-PImage fillMask;
-
+//===========/----------------------------------------------
+//  [_PRO]  /  Processing
+//=========/------------------------------------------------
 
 void setup()
 {
 	size(640, 640, P2D);
 	background(BG_COLOR);
 
-	// set lines
-	linesColor = loadImage(IMG_URL);
-	linesColor.filter(INVERT);
+	//
+	centerX = int(width * 0.5);
+	centerY = int(height * 0.5);
 
-	// set fill
-	fillColor = loadImage(FLAT_COLOR_BLOCK_URL);
-	fillMask = loadImage(IMG_URL);
+	// set source
+	source = loadImage(IMG_URL);
+	source.loadPixels();
+
+	// var relative to source
+	int numPixels = source.pixels.length;
+	setTargetDimensions(source.width, source.height);
+
+	// instantiate fill color
+	linesImage = createImage	(
+								source.width, 
+								source.height,
+								ARGB
+							);
+	linesImage.loadPixels();
+
+	// instantiate lines color
+	fillImage = createImage	(
+									source.width, 
+									source.height,
+									ARGB
+								);
+	fillImage.loadPixels();
+
+	// loop through source
+	int threshold = 128;
+	for(int i = 0; i < numPixels; i++)
+	{
+		// fill color
+		linesImage.pixels[i] = color	(
+											red(LINES_COLOR),
+											green(LINES_COLOR),
+											blue(LINES_COLOR),
+											alpha(source.pixels[i])
+										); 
+
+		// lines color
+		if	(
+				red(source.pixels[i]) > threshold 
+				&& green(source.pixels[i]) > threshold
+				&& blue(source.pixels[i]) > threshold
+			)
+		{
+			fillImage.pixels[i] = FILL_COLOR;
+		}
+	}
+	linesImage.updatePixels();
 }
 
 
@@ -28,14 +80,45 @@ void draw()
 {
 	background(BG_COLOR);
 
-	// draw lines
-	// image(linesColor, width * 0.5, height * 0.5);
-	// tint(240, 0, 0);
-
-	// draw colors
-	image(fillColor, width * 0.5, height * 0.5);
-	fillColor.mask(fillMask);
-	tint(0, 0, 240);
+	image	(
+				linesImage, 
+				centerX, 
+				centerY,
+				targetDimensions[0],
+				targetDimensions[1]
+			);
+	image	(
+				fillImage, 
+				centerX, 
+				centerY,
+				targetDimensions[0],
+				targetDimensions[1]
+			);
 
 	imageMode(CENTER);
+}
+
+
+//===========/----------------------------------------------
+//  [_MTD]  /  Methods
+//=========/------------------------------------------------
+
+void setTargetDimensions(int w, int h)
+{
+	targetDimensions = new int[2];
+
+	float MAX_SOURCE_SIZE = 1920;
+	float TARGET_SIZE = 480;
+	float RATIO = TARGET_SIZE / MAX_SOURCE_SIZE;
+
+	if(w > h)
+	{
+		targetDimensions[0] = int(TARGET_SIZE);
+		targetDimensions[1] = int(h * RATIO);
+	}
+	else
+	{
+		targetDimensions[0] = int(w * RATIO);
+		targetDimensions[1] = int(TARGET_SIZE);
+	}
 }
